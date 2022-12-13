8395b456d59fac53471a140fcb0484d409c6e727
Luke P <luke.ponssen@icloud.com>
2022-12-12 20:14:00 -0800

    add more photos and projects

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,6 +6,9 @@ import swimmer from '../images/swimmer.png'
 import familymember from '../images/family.png'
 import elisa from '../images/elisa.png'
 import musician from '../images/piano.png'
+import friend from '../images/friend.png'
+import engineer from '../images/engineer.png'
+import reader from '../images/reader.png'
 
 function AboutMe() {
 return (
@@ -29,12 +32,24 @@ return (
             <img src={familymember} className="about-me-img" alt="familymember" />
         </div>
         <div>
-            <h3>I'm a partner</h3>
-            <img src={elisa} className="about-me-img" alt="partner" />
+            <h3>I'm an engineer</h3>
+            <img src={engineer} className="about-me-img" alt="engineer" />
         </div>
         <div>
             <h3>I'm a musician</h3>
             <img src={musician} className="about-me-img" alt="musician" />
+        </div>
+        <div>
+            <h3>I'm a reader</h3>
+            <img src={reader} className="about-me-img" alt="reader" />
+        </div>
+        <div>
+            <h3>I'm a friend</h3>
+            <img src={friend} className="about-me-img" alt="friend" />
+        </div>
+        <div>
+            <h3>I'm a partner</h3>
+            <img src={elisa} className="about-me-img" alt="partner" />
         </div>
       </div>
     </div>
