8936df42f1da004be9c4915c94c1b53fde308f85
lukep <luke.ponssen@icloud.com>
2023-04-10 11:08:29 -0700

    remove chatbot, backend unsupported

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navbar from './components/Navbar.js'
 import Home from './pages/Home.js'
 import AboutMe from './pages/AboutMe.js'
 import Projects from './pages/Projects.js'
-import Chatbot from './pages/Chatbot.js'
 import { Route, Routes } from 'react-router-dom'
 import {Helmet} from "react-helmet"
 import React, { useEffect } from "react"
